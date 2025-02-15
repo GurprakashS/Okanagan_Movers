29ca24e74c2fe10391a1cfc00785b1973b82578a
GurprakashS <gsinghatwork@gmail.com>
2025-02-14 20:24:10 -0800

    updating code if it is not added to git

---
+++ src/Components/Testimonial/testimonials.jsx
@@ -0,0 +1,29 @@
+// src/Components/Testimonial/testimonials.jsx
+import React from "react";
+
+const Testimonials = () => {
+  const testimonials = [
+    {
+      name: "John Doe",
+      review: "Great service! They made my move stress-free.",
+    },
+    { name: "Jane Smith", review: "Highly recommend their packing services." },
+    { name: "Mike Johnson", review: "Professional and affordable." },
+  ];
+
+  return (
+    <div className="testimonials">
+      <h2>Whats our Customers Say</h2>
+      <div className="testimonial-cards">
+        {testimonials.map((testimonial, index) => {
+          <div key={index} className="testimonial">
+            <p>{testimonial.review}</p>
+            <p>{testimonial.name}</p>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
