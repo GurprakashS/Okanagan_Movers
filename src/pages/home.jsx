10a88728e2102b67d493a676d32fdd8addad75e9
GurprakashS <gsinghatwork@gmail.com>
2025-02-16 21:43:41 -0800

    horizontal scrolling effect is implemented

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,14 @@
 // src/Pages/home.jsx
-import { motion } from "framer-motion";
+import ScrollingRow from "../Components/ScrollingEffect/scrolling";
 
 const Home = () => {
   return (
-    <div className="home">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="hero"
-      >
-        <h1>Your Move, Made Simple</h1>
-        <p>Reliable and Affordable Packing & Moving Services</p>
-        <button className="cta-button">Get a Free Quote</button>
-      </motion.div>
-    </div>
+    <>
+      <div className="home"></div>
+      <div>
+        <ScrollingRow />
+      </div>
+    </>
   );
 };
 
